4a3aadd74a4c9db7a7a4ae466f65e94295b49238
KadirQuintero03 <kadireq1928@gmail.com>
2024-08-25 18:12:03 -0500

    Vista de la homepage completada

---
+++ pages/public/HomePage.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomePage() {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
