9505c20584189fb0f94929b0512a34784088e0df
KadirQuintero03 <kadireq1928@gmail.com>
2024-08-28 19:04:27 -0500

    Se creo un archivo de estilos para home page y se arreglaron los estilos de la misma home page

--- app/_createaccount.js
+++ app/_createaccount.js
@@ -1,10 +1,5 @@
-import { View } from "react-native";
 import CreateAccountPage from "../pages/public/CreateAccountPage";
 
 export default function _createaccount() {
-  return (
-    <View>
-      <CreateAccountPage />
-    </View>
-  );
+  return <CreateAccountPage />;
 }
