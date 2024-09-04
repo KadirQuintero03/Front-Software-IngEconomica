3ebd6f1f96ce52b7246b75b10d3ce3eab3cf376f
david123456858 <juandavidperaltafuentes@gmail.com>
2024-09-03 22:01:56 -0500

    con el homePage

--- app/index.js
+++ app/index.js
@@ -1,12 +1,11 @@
 import { View } from "react-native";
-//import HomePage from "../pages/public/HomePage";
+import HomePage from "../pages/public/HomePage";
 import { pixels } from "../stores/usePhoneProperties";
-import MainPage from "../pages/private/MainPage";
 
 export default function Index() {
   return (
     <View style={{ marginTop: pixels(25) }}>
-      <MainPage />
+      <HomePage />
     </View>
   );
 }
