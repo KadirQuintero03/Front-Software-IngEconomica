d49c3d5270c0b9b17bc0b1afd83a0509d4de0b1f
Jesus Bermudez <jesmanuelber@gmail.com>
2024-09-03 20:04:57 -0500

    Login terminado (en 2m)

--- pages/public/LoginPage.jsx
+++ pages/public/LoginPage.jsx
@@ -1,9 +1,33 @@
-import { View, Text, Image, TextInput, Pressable } from "react-native";
+import { View, Text, Image, TextInput, Pressable, Alert } from "react-native";
 import { styles } from "../public/loginPageStyle";
 import logocarter from "../../assets/logocarter.png";
 import { PhoneIcon, LockIcon } from "../../components/icons/Icons";
+import { useState } from "react";
+import { login } from "../../services/LoginServices";
+import { useRouter } from "expo-router";
 
 export default function LoginPage() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const response = await login({
+        phoneNumber: phoneNumber,
+        password: password,
+      });
+
+      if (response.hasOwnProperty("error")) {
+        Alert.alert("Error", response.error);
+      }
+
+      router.navigate("./_mainPage");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={logocarter}></Image>
@@ -13,6 +37,8 @@ export default function LoginPage() {
         <TextInput
           placeholder="Número de Teléfono"
           keyboardType="numeric"
+          onChangeText={setPhoneNumber}
+          value={phoneNumber}
           style={styles.input}
         />
       </View>
@@ -22,18 +48,36 @@ export default function LoginPage() {
         <TextInput
           placeholder="Contraseña"
           secureTextEntry
+          onChangeText={setPassword}
+          value={password}
           style={styles.input}
         />
       </View>
 
-      <Pressable style={styles.btnLogin}>
+      <Pressable
+        onPress={handleLogin}
+        style={
+          (({ pressed }) => [
+            { backgroundColor: pressed ? "#00000026" : undefined },
+          ],
+          styles.btnLogin)
+        }
+      >
         <Text style={styles.txtLogin}>Iniciar sesion</Text>
       </Pressable>
 
       <Text style={styles.txtMsg}>¿Olvidaste tu contraseña?</Text>
       <Text style={styles.txtMsg}>
         Presiona{" "}
-        <Text style={{ color: "#022a9b", fontWeight: "bold" }}>aquí</Text>
+        <Text
+          style={{
+            color: "#022a9b",
+            fontWeight: "bold",
+            textDecorationLine: "underline",
+          }}
+        >
+          aquí
+        </Text>
       </Text>
     </View>
   );
---
+++ stores/useApi.js
@@ -0,0 +1,1 @@
+export const apiUrl = "https://api-software-economico.up.railway.app/api/v1";
