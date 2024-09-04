df902ac1eea799a0f30c86012423e895a519328e
david123456858 <juandavidperaltafuentes@gmail.com>
2024-09-03 21:59:08 -0500

    Algunas cosas de react

--- app/index.js
+++ app/index.js
@@ -1,11 +1,12 @@
 import { View } from "react-native";
-import HomePage from "../pages/public/HomePage";
+//import HomePage from "../pages/public/HomePage";
 import { pixels } from "../stores/usePhoneProperties";
+import MainPage from "../pages/private/MainPage";
 
 export default function Index() {
   return (
     <View style={{ marginTop: pixels(25) }}>
-      <HomePage />
+      <MainPage />
     </View>
   );
 }
