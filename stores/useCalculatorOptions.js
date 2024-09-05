eda8e0febd043c4ab7c174f428c10a228d3dfc71
Jesus Bermudez <jesmanuelber@gmail.com>
2024-09-05 00:35:53 -0500

    .

--- stores/useCalculatorOptions.js
+++ stores/useCalculatorOptions.js
@@ -8,19 +8,18 @@ let useCalculatorOption;
 export const optionList = [
   { id: 0, name: "Interes Simple", component: <InteresSimple /> },
   { id: 1, name: "Interes Compuesto", component: <InteresSimple /> },
-  { id: 2, name: "Anualidades", component: <Template /> },
-  { id: 3, name: "Gradiente Aritmetico", component: <GradienteGeometrico /> },
-  { id: 4, name: "Gradiente Geometrico", component: <GradienteGeometrico /> },
-  { id: 5, name: "Amortización", component: <Template /> },
-  { id: 6, name: "Taza de interés de retorno", component: <Template /> },
-  { id: 7, name: "Unidad de valor real", component: <Template /> },
+  { id: 2, name: "Gradiente Aritmetico", component: <GradienteAritmetico /> },
+  { id: 3, name: "Gradiente Geometrico", component: <GradienteGeometrico /> },
+  { id: 4, name: "Amortización", component: <Template /> },
+  { id: 5, name: "Taza de interés de retorno", component: <Template /> },
+  { id: 6, name: "Unidad de valor real", component: <Template /> },
   {
-    id: 8,
+    id: 7,
     name: "Evaluación de alternativas de Inversión",
     component: <Template />,
   },
-  { id: 9, name: "Inflación", component: <Template /> },
-  { id: 10, name: "Bonos", component: <Template /> },
+  { id: 8, name: "Inflación", component: <Template /> },
+  { id: 9, name: "Bonos", component: <Template /> },
 ];
 
 export const getCalculatorOption = () => useCalculatorOption;
