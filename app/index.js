f3ee6fb2d2ac3e5f900f94d5860aaef94218772a
KadirQuintero03 <kadireq1928@gmail.com>
2024-10-08 19:20:15 -0500

    Subiendo anualidades

--- app/index.js
+++ app/index.js
@@ -1,11 +1,9 @@
 import { View } from "react-native";
 import HomePage from "../pages/public/HomePage";
-import MainPage from "../pages/private/MainPage";
-
 export default function Index() {
   return (
     <View>
-      <MainPage />
+      <HomePage />
     </View>
   );
 }
